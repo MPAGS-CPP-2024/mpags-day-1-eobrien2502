993f2595cf19528740abe0d26108c87eec77f4a7
Ellie O'Brien <eobrien4@sheffield.ac.uk>
2024-10-11 15:10:34 +0000

    Update mpags-cipher to print out command line arguments

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -2,42 +2,94 @@
 
 #include <iostream>
 #include <string>
-int main()
-{
-    int a_whole_num{3};
-    a_whole_num = 4;
-    std::cout << a_whole_num << std::endl;
+#include <vector>
 
-    double decimal_num{1.2};
-    decimal_num = 1.6;
-    std::cout << decimal_num << std::endl;
+int main( int argc, char* argv[]){
 
-    // int another_num{decimal_num*2};
+    const std::vector<std::string> cmdLineArgs{argv, argv+argc};
 
-    const double c_dbl{4.8};
-    std::cout << c_dbl << std::endl;
+    // printing out the command line arguments
+    for (std::size_t i{0}; i < cmdLineArgs.size(); i++){
+        std::cout << cmdLineArgs[i] << std::endl;
 
-    int int_num_2{7};
-    int_num_2 = 3;
+        /*if(cmdLineAgrs[i] == "--help"){
+            std::cout << "This C++ program is used to encrypt and decrypt text. \n ./file_name -i input_file -o output_file -c cipher -k key encryp/decrypt" << std::endl;
+        }
+        else if(cmdLineAgrs[i] == "--h"){
+            std::cout << "This C++ program is used to encrypt and decrypt text. \n ./file_name -i input_file -o output_file -c cipher -k key encryp/decrypt" << std::endl;
+        
+        
+        } */
+    }
+
+    char in_char{'x'};
+    std::string out_str{""};
+
+    // Take each letter from user input and in each case:
+    while(std::cin >> in_char)
+    {
+            
+        //- Convert to upper case
+        if(std::isalpha(in_char)){
+            out_str += std::toupper(in_char);
+            continue;
+        }
+
+        //- Change numbers to words
+        switch(in_char){
+
+            case '0':
+                out_str += "ZERO";
+                break;
+            
+            case '1':
+                out_str += "ONE";
+                break;
+            
+            case '2':
+                out_str += "TWO";
+                break;
+            
+            case '3':
+                out_str += "THREE";
+                break;
+
+            case '4':
+                out_str += "FOUR";
+                break;
+
+            case '5':
+                out_str += "FIVE";
+                break;
+
+            case '6':
+                out_str += "SIX";
+                break;
+
+            case '7':
+                out_str += "SEVEN";
+                break;
+
+            case '8':
+                out_str += "EIGHT";
+                break;
+
+            case '9':
+                out_str += "NINE";
+                break;
+            
+            default:
+                // Do nothing
+                break;
+        }
+
+        //- Ignore any other (non-alpha) characters
+        //- In each case, add result to a string variable
+    }
 
 
-    std::string my_str {"My String"};
-    std::cout << my_str << std::endl;
 
-    int a{5};
-    double d{7.2};
-    double product{a*d};
-    std::cout << product << std::endl;
-
-    double division{d/a};
-    std::cout << division << std::endl;
-
-    //double div_ints{a/a_whole_num};
-    //std::cout << div_ints << std::endl;
-
-    std::string str2 {"weather"};
-    char letter{'o'};
-    letter = str2[3];
-    std::cout << letter << std::endl;
+    // print out the new string
+    std::cout << out_str << std::endl;
     
 }
