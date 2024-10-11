187cfd0fc657af1f39216025e1c3dee8a1f95d41
Ellie O'Brien <eobrien4@sheffield.ac.uk>
2024-10-11 12:49:10 +0000

    Update mpags-cipher after learning about types and strings

---
+++ mpags-cipher.cpp
@@ -0,0 +1,27 @@
+// mpags-cipher.cpp
+
+#include <iostream>
+#include <string>
+int main()
+{
+    int a_whole_num{3};
+    a_whole_num = 4;
+    std::cout << a_whole_num << std::endl;
+
+    double decimal_num{1.2};
+    decimal_num = 1.6;
+    std::cout << decimal_num << std::endl;
+
+    // int another_num{decimal_num*2};
+
+    const double c_dbl{4.8};
+    std::cout << c_dbl << std::endl;
+
+    int int_num_2{7};
+    int_num_2 = 3;
+
+
+    std::string my_str {"My String"};
+    std::cout << my_str << std::endl;
+    
+}
