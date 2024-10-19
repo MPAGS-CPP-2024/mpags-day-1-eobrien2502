74ae7f3e65f44f6bc7becda66a3722365aebf873
Ellie O'Brien <eobrien4@sheffield.ac.uk>
2024-10-19 12:46:37 +0000

    Improvements after recieving feedback

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -14,83 +14,76 @@ int main( int argc, char* argv[]){
     // Creating a boolean variable to tell me when the version is requested.
     bool versionRequested{false};
 
-    // Creating a boolean to check if the input file was given and initialising the input file variable
-    bool input_fileGiven{false};
+    // Initialising the input file variable
     std::string input_file{""};
 
-    // Creating a boolean to check if the output file was given and initialising the output file variable
-    bool output_fileGiven{false};
+    // Initialising the output file variable
     std::string output_file{""};
 
-    // Looking through the command line arguments, excluding the first one.
-    for (std::size_t i{1}; i < cmdLineArgs.size(); i++){
+    // Initialising the command line argument vector size variable
+    const std::size_t ncmdLineArgs{cmdLineArgs.size()};
 
-        //std::cout <<  " The argument being considered now is " << cmdLineArgs[i] << std::endl;
-        //std::cout <<  "The argument index is " << i << std::endl;
+    // Looking through the command line arguments, excluding the first one.
+    for (std::size_t i{1}; i < ncmdLineArgs; i++){
 
         //Looking for whether help is requested
         if((cmdLineArgs[i] == "--help") || (cmdLineArgs[i] == "-h") ){
 
             helpRequested = true;
+            // So that when a request for help is found, none of the other options are looked at
+            // and the programme ends.
+            break;
 
-        }
-
-        // Looking for whether the version is requested
-        if(cmdLineArgs[i] == "--version" ){
-
+        } else if(cmdLineArgs[i] == "--version" ){
+            // Looking for whether the version is requested
             versionRequested = true;
+            // So that when a request for the version is seen (without help), the version if given and no other options are looked at
+            // and the programme ends.
+            break;
 
-        }
+        } else if(cmdLineArgs[i] == "-i" ){
+            // Looking for whether the option -i has been used as an argument
 
-
-        // Looking for whether the option -i has been used as an argument
-        if(cmdLineArgs[i] == "-i" ){
-            
             // Need to look to see if this was the last argument specified
             // If it was, need to return an error message.
-            if (i == (cmdLineArgs.size() - 1)){
-                std::cout << "An input file must be specified in the -i option after '-i'." << std::endl;
-                break;
+            if (i == (ncmdLineArgs - 1)){
+                std::cerr << "[error] An input file must be specified in the -i option after '-i'." << std::endl;
+                return 1;
             }
             else{
                 // If it wasn't, proceed as normal.
-
-                // Take the next argument as the input file
-                input_fileGiven = true;
-                //std::cout << input_fileGiven << std::endl;
+                
                 input_file = cmdLineArgs[i+1];
-                //std::cout << "The file identified as an input file is " << input_file << std::endl;
+                // To skip over the next argument as we know it will be the input file name.
+                i++;
+                
 
             }
 
-            
-        }
-        
-
-
-
-        // Looking for whether the option -o has been used as an argument
-        if(cmdLineArgs[i] == "-o" ){
+        } else if(cmdLineArgs[i] == "-o" ){
+            // Looking for whether the option -o has been used as an argument
 
             // Need to look to see if this was the last argument specified
             // If it was, need to return an error message.
-            if (i == (cmdLineArgs.size() - 1)){
-                std::cout << "An output file must be specified in the -o option after '-o'." << std::endl;
-                break;
+            if (i == (ncmdLineArgs - 1)){
+                std::cout << "[error] An output file must be specified in the -o option after '-o'." << std::endl;
+                return 1;
             }
             else{
                 // If it wasn't, proceed as normal.
             
                 // Take the next argument as the output file
-                output_fileGiven = true;
                 output_file = cmdLineArgs[i+1];
-                //std::cout << "The file identified as an output file is " << output_file << std::endl;
+                // Skipping over the next argument as we know that its the output file name.
+                i++;
             }
-           
-        }
-        
 
-        
+        } else{
+            // If an unknown argument is given
+            std::cerr << "[error] A disallowed argument was given. Use -h/--help to see the available allowed arguments." << std::endl;
+            return 1;
+        }
+
         
     }
 
@@ -116,6 +109,7 @@ int main( int argc, char* argv[]){
         << " -k <key>               Specify the key\n"
         << " encrypt/decrypt        Specify if the message is for encryption or decryption\n"
         << std::endl;
+        return 0;
 
     
     }
@@ -127,12 +121,13 @@ int main( int argc, char* argv[]){
         << " \n"
         << " version: v0.1.0\n" 
         << std::endl;
+        return 0;
 
         
     }
 
     // The input file 
-    if (input_fileGiven) {
+    if (!input_file.empty()) {
 
         // At the moment, this will just be a print statement
         std::cout << "The input file to the program is " << input_file << " ." << std::endl;
@@ -141,11 +136,10 @@ int main( int argc, char* argv[]){
     else{
         std::cout << "The input file must be specified. Use -h / --help for more information." << std::endl;
          
-
     }
 
     // The output file
-    if (output_fileGiven) {
+    if (!output_file.empty()) {
 
         // At the moment, this will just be a print statement
         std::cout << "The output file to the program is " << output_file << " ." << std::endl;
@@ -154,7 +148,6 @@ int main( int argc, char* argv[]){
     else{
 
         std::cout << "The output file must be specified. Use -h / --help for more information." << std::endl;
-         
 
     }
 
@@ -242,3 +235,5 @@ int main( int argc, char* argv[]){
 //
 // I also kept the code for the user input from the terminal just because it worked and it doesn;t affect the other part of the code.
 // I'd be greatful for your feedback and suggestions on how to improve my work and solve the problems I've stated.
+//
+// Corrected this code, considering the feedback I recieved. 
